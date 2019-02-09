2cd2c093dc89113478f748ea8d32c01cd96ceaab
Arhell <arhell333@gmail.com>
2019-02-09 21:25:41 +0200

    upd section

--- gulp/path/tasks.js
+++ gulp/path/tasks.js
@@ -2,7 +2,7 @@ module.exports = [
   './gulp/tasks/browser-sync',
   './gulp/tasks/img',
   './gulp/tasks/pug',
-  //'./gulp/tasks/scripts',
+  './gulp/tasks/scripts',
   './gulp/tasks/stylus',
   './gulp/tasks/watch'
 ];
